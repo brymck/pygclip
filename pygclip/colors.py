8d66befee3e11fb187ea56682aeec5480afc0d98
Bryan McKelvey <bryan.mckelvey@gmail.com>
2018-02-11 23:52:46 +0900

    Address Python 3.5 issues with type coercion syntax

--- pygclip/colors.py
+++ pygclip/colors.py
@@ -15,8 +15,8 @@ RGB = Tuple[int, int, int]
 
 def _hex_to_rgb(hex_color: str) -> RGB:
     hex_color = hex_color.lstrip('#')
-    rgb: RGB = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-    return rgb
+    red, green, blue = [int(hex_color[i:i+2], 16) for i in (0, 2, 4)]
+    return red, green, blue
 
 
 def _rgb_to_hex(rgb: RGB) -> str:
@@ -24,8 +24,8 @@ def _rgb_to_hex(rgb: RGB) -> str:
 
 
 def _darken_rgb(rgb: RGB, factor: float) -> RGB:
-    rgb: RGB = tuple(int(x * (1 - factor)) for x in rgb)
-    return rgb
+    red, green, blue = [int(x * (1 - factor)) for x in rgb]
+    return red, green, blue
 
 
 def darken_color(hex_color: str, factor: float) -> str:
