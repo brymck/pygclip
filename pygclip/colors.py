aec1244195f19ea4323a7252f934a69223811420
Bryan McKelvey <bryan.mckelvey@gmail.com>
2018-02-11 23:43:22 +0900

    Clean up code style

--- pygclip/colors.py
+++ pygclip/colors.py
@@ -15,7 +15,8 @@ RGB = Tuple[int, int, int]
 
 def _hex_to_rgb(hex_color: str) -> RGB:
     hex_color = hex_color.lstrip('#')
-    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+    rgb: RGB = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+    return rgb
 
 
 def _rgb_to_hex(rgb: RGB) -> str:
@@ -23,7 +24,8 @@ def _rgb_to_hex(rgb: RGB) -> str:
 
 
 def _darken_rgb(rgb: RGB, factor: float) -> RGB:
-    return tuple(int(x * (1 - factor)) for x in rgb)
+    rgb: RGB = tuple(int(x * (1 - factor)) for x in rgb)
+    return rgb
 
 
 def darken_color(hex_color: str, factor: float) -> str:
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,4 +1,3 @@
-import logging
 from pygclip.logging import setup_logging
 
 
